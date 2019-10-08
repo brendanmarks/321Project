bd2868dc5786c8ccb3c5999fdcdc4f86762d84a5
Brendan Marks <brendan99@live.ca>
2019-10-07 20:37:05 -0400

    setup with gradle

---
+++ src/main/java/ca/mcgill/ecse321/tutoringsystem/TutoringApp.java
@@ -0,0 +1,14 @@
+package ca.mcgill.ecse321.tutoringsystem;
+
+public class TutoringApp {
+
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	//create view
+            }
+        });
+	}
+
+}
---
+++ src/test/java/ca/mcgill/ecse321/tutoringsystem/TutoringAppTest.java
@@ -0,0 +1,10 @@
+package ca.mcgill.ecse321.tutoringsystem;
+
+public class TutoringAppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
