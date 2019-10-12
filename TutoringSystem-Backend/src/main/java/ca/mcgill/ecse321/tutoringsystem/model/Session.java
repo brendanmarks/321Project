dc45721504ca7a2f9d679a03e60c773bf875c50c
samihilal1234 <sami.hilal@mail.mcgill.ca>
2019-10-12 14:40:57 -0400

    added date and time

--- TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Session.java
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Session.java
@@ -5,6 +5,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import java.util.Set;
 import javax.persistence.OneToMany;
+import java.sql.Time;
+import java.sql.Date;
 
 @Entity
 public class Session{
@@ -17,48 +19,72 @@ public void setSessionId(String value) {
 public String getSessionId() {
     return this.sessionId;
 }
-   private Tutorial tutorial;
-   
-   @OneToOne(optional=false)
-   public Tutorial getTutorial() {
-      return this.tutorial;
-   }
-   
-   public void setTutorial(Tutorial tutorial) {
-      this.tutorial = tutorial;
-   }
-   
-   private Set<Student> student;
-   
-   @OneToMany(mappedBy="session" )
-   public Set<Student> getStudent() {
-      return this.student;
-   }
-   
-   public void setStudent(Set<Student> students) {
-      this.student = students;
-   }
-   
-   private Set<Review> review;
-   
-   @OneToMany
-   public Set<Review> getReview() {
-      return this.review;
-   }
-   
-   public void setReview(Set<Review> reviews) {
-      this.review = reviews;
-   }
-   
-   private Bill bill;
-   
-   @OneToOne(optional=false)
-   public Bill getBill() {
-      return this.bill;
-   }
-   
-   public void setBill(Bill bill) {
-      this.bill = bill;
-   }
-   
-   }
+private Tutorial tutorial;
+
+@OneToOne(optional=false)
+public Tutorial getTutorial() {
+   return this.tutorial;
+}
+
+public void setTutorial(Tutorial tutorial) {
+   this.tutorial = tutorial;
+}
+
+private Set<Student> student;
+
+@OneToMany(mappedBy="session" )
+public Set<Student> getStudent() {
+   return this.student;
+}
+
+public void setStudent(Set<Student> students) {
+   this.student = students;
+}
+
+private Set<Review> review;
+
+@OneToMany
+public Set<Review> getReview() {
+   return this.review;
+}
+
+public void setReview(Set<Review> reviews) {
+   this.review = reviews;
+}
+
+private Bill bill;
+
+@OneToOne(optional=false)
+public Bill getBill() {
+   return this.bill;
+}
+
+public void setBill(Bill bill) {
+   this.bill = bill;
+}
+
+private Time startTime;
+
+public void setStartTime(Time value) {
+    this.startTime = value;
+}
+public Time getStartTime() {
+    return this.startTime;
+}
+private Time endTime;
+
+public void setEndTime(Time value) {
+    this.endTime = value;
+}
+public Time getEndTime() {
+    return this.endTime;
+}
+private Date date;
+
+public void setDate(Date value) {
+    this.date = value;
+}
+public Date getDate() {
+    return this.date;
+}
+}
