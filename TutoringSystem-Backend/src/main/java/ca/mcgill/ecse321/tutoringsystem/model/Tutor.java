421a0bffc1336517b8f66e76904815a64a03bebc
samihilal1234 <sami.hilal@mail.mcgill.ca>
2019-10-12 16:35:14 -0400

    dao added

--- TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Tutor.java
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Tutor.java
@@ -3,6 +3,7 @@ package ca.mcgill.ecse321.tutoringsystem.model;
 import javax.persistence.Entity;
 import java.util.Set;
 import javax.persistence.OneToMany;
+import java.sql.Time;
 
 @Entity
 public class Tutor extends Person{
@@ -17,4 +18,20 @@ public class Tutor extends Person{
       this.tutorial = tutorials;
    }
    
-   }
+   private double hourlyRate;
+
+public void setHourlyRate(double value) {
+    this.hourlyRate = value;
+}
+public double getHourlyRate() {
+    return this.hourlyRate;
+}
+private Time availability;
+
+public void setAvailability(Time value) {
+    this.availability = value;
+}
+public Time getAvailability() {
+    return this.availability;
+}
+}
