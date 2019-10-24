a19ac36c6bdb0b85362ef0a0572686f9977e375c
Sean Smith <43576557+ssmith21@users.noreply.github.com>
2019-10-23 21:58:15 -0400

    Update Sessions DTO
    added assigned tutor dto and list of dto students in session

--- TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/SessionDto.java
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/SessionDto.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse321.tutoringsystem.dto;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.ArrayList;
 
 public class SessionDto {
 	
@@ -9,19 +10,23 @@ public class SessionDto {
 	private Time startTime;
 	private Time endTime;
 	private Date date;
+	private TutorDto assignedTutor;
+	private static ArrayList<StudentDto> registeredStudents;
 	
+
 	public SessionDto() {
 	}
 	
 	public SessionDto(String sessionId) {
-		this(sessionId, Date.valueOf("1971-01-01"), Time.valueOf("00:00:00"), Time.valueOf("23:59:59"));
+		this(sessionId, Date.valueOf("1971-01-01"), Time.valueOf("00:00:00"), Time.valueOf("23:59:59"), registeredStudents);
 	}
 	
-	public SessionDto(String sessionId, Date date, Time startTime, Time endTime) {
+	public SessionDto(String sessionId, Date date, Time startTime, Time endTime, ArrayList<StudentDto> registeredStudents) {
 		this.sessionId = sessionId;
 		this.date = date;
 		this.startTime = startTime;
 		this.endTime = endTime;
+		SessionDto.registeredStudents = registeredStudents;
 	}
 	
 	public String getSessionId() {
@@ -39,6 +44,24 @@ public class SessionDto {
 	public Time getEndTime() {
 		return endTime;
 	}
+	
+	public ArrayList<StudentDto> getRegisteredStudents() {
+		return registeredStudents;
+	}
+
+	public void setRegisteredStudents(ArrayList<StudentDto> registeredStudents) {
+		SessionDto.registeredStudents = registeredStudents;
+	}
+	
+	public TutorDto getAssignedTutor() {
+		return assignedTutor;
+	}
+
+	public void setAssignedTutor(TutorDto assignedTutor) {
+		this.assignedTutor = assignedTutor;
+	}
+	
+
 
 }
 
