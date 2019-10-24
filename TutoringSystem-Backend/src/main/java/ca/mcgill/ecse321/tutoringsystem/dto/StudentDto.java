c3c72031f7c9efa1426ab5c43850db8d586fa309
Sean Smith <43576557+ssmith21@users.noreply.github.com>
2019-10-23 22:01:27 -0400

    Updated StudentDTO
    Added an arrayList of SessionDTOs so that the student can see their list of sessions

--- TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/StudentDto.java
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/StudentDto.java
@@ -1,22 +1,25 @@
 package ca.mcgill.ecse321.tutoringsystem.dto;
 
+import java.util.ArrayList;
 
 public class StudentDto {
 	private String name;
 	private String email;
 	private String username;
 	private String password;
-	
+	private static ArrayList<SessionDto> sessions;
+
 	public StudentDto() {
 	}
 	
 	//here we would want the student to get all his sessions
 
-	public StudentDto(String name, String email, String username, String password) {
+	public StudentDto(String name, String email, String username, String password, ArrayList<SessionDto> sessions) {
 		this.name = name;
 		this.email = email;
 		this.username = username;
 		this.password = password;
+		StudentDto.sessions = sessions;
 	}
 	
 	public String getName() {
@@ -34,5 +37,14 @@ public class StudentDto {
 	public String getEndTime() {
 		return password;
 	}
+	
+	public static ArrayList<SessionDto> getSessions() {
+		return sessions;
+	}
+
+	public static void setSessions(ArrayList<SessionDto> sessions) {
+		StudentDto.sessions = sessions;
+	}
+
 
 }
