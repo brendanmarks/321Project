73cc7bc555f7d388f6990f32bf5d9c0023f94607
Sean Smith <43576557+ssmith21@users.noreply.github.com>
2019-10-23 22:03:29 -0400

    Updated TutorDto
    Added ArrayList of SessionDtos so the tutor can see what sessions they have

--- TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/TutorDto.java
+++ TutoringSystem-Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/TutorDto.java
@@ -1,20 +1,24 @@
 package ca.mcgill.ecse321.tutoringsystem.dto;
 
+import java.util.ArrayList;
+
 public class TutorDto {
 	
 	private String name;
 	private String email;
 	private String username;
 	private String password;
-	
+	private ArrayList<SessionDto> sessions;
+
 	public TutorDto() {
 	}
 
-	public TutorDto(String name, String email, String username, String password) {
+	public TutorDto(String name, String email, String username, String password, ArrayList<SessionDto> sessions) {
 		this.name = name;
 		this.email = email;
 		this.username = username;
 		this.password = password;
+		this.sessions = sessions;
 	}
 	
 	public String getName() {
@@ -31,5 +35,13 @@ public class TutorDto {
 
 	public String getEndTime() {
 		return password;
+	}
+	
+	public ArrayList<SessionDto> getSessions() {
+		return sessions;
+	}
+
+	public void setSessions(ArrayList<SessionDto> sessions) {
+		this.sessions = sessions;
 	}
 }
