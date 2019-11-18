3429c2dea205b7485b8438a6d13541566d925d57
Brendan Marks <brendan99@live.ca>
2019-11-18 13:10:42 -0500

    fix endpoints for deployed frontend cause SOMEPPL forgot

--- tutoringsystem-frontend/src/components/pickCourseByName.vue
+++ tutoringsystem-frontend/src/components/pickCourseByName.vue
@@ -67,11 +67,24 @@
 import axios from "axios";
 var config = require("../../config");
 
-var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
-var backendUrl =
-  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
-console.log(frontendUrl);
-console.log(backendUrl);
+let frontendUrlConfig = function () {
+    if (process.env.NODE_ENV === 'production') {
+        return 'https://' + config.build.host + ':' + config.build.port
+    }
+    else {
+        return 'http://' + config.dev.host + ':' + config.dev.port
+    }
+}
+let backendUrlConfig = function () {
+    if (process.env.NODE_ENV === 'production') {
+        return 'https://' + config.build.backendHost + ':' + config.build.backendPort
+    }
+    else {
+        return 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+    }
+}
+var frontendUrl = frontendUrlConfig()
+var backendUrl = backendUrlConfig()
 var AXIOS = axios.create({
   baseURL: backendUrl,
   headers: { "Access-Control-Allow-Origin": frontendUrl }
