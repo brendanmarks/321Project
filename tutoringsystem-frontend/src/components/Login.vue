6eeca5275727075454fc045b2bd11ed306f3ae90
samihilal1234 <sami.hilal@mail.mcgill.ca>
2019-11-26 16:12:48 -0500

    new sign up login

--- tutoringsystem-frontend/src/components/Login.vue
+++ tutoringsystem-frontend/src/components/Login.vue
@@ -1,89 +1,198 @@
 <template>
-  <div id="login">
-    <div class="site-section site-hero inner">
-      <div class="container-fluid" id="top-container">
-        <div class="container text-center" id="img-container">
-          <img
-            src="https://ballstateeconomics.files.wordpress.com/2014/04/tutoring-banner.png"
-            width="500"
-            height="100"
-          />
-        </div>
-      </div>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Sign In</h5>
+            <form class="form-signin">
+              <div class="form-label-group">
+                <input
+                  type="email"
+                  id="inputEmail"
+                  class="form-control"
+                  placeholder="Username"
+                  required
+                  autofocus
+                  v-model="username"
+                />
+                <label for="inputEmail">Username</label>
+              </div>
 
-      <br />
-      <br />
-      <br />
-      <br />
-      <div class="half left">
-        <div class="container" align="center">
-          <div class="row align-items-center" align="center">
-            <div class="col-md-0" align="center" style="padding-left:10%">
-              <h1 class="d-block mb-0"></h1>
-            </div>
+              <div class="form-label-group">
+                <input
+                  type="password"
+                  id="inputPassword"
+                  class="form-control"
+                  placeholder="Password"
+                  required
+                  v-model="password"
+                />
+                <label for="inputPassword">Password</label>
+              </div>
+
+              <div class="custom-control custom-checkbox mb-3">
+                <input type="checkbox" class="custom-control-input" id="customCheck1" />
+                <label class="custom-control-label" for="customCheck1">Remember password</label>
+              </div>
+              <input
+                @click="login(username, password)"
+                type="submit"
+                value="Login"
+                class="btn btn-lg btn-primary btn-block text-uppercase"
+              />
+              <hr class="my-4" />
+              <input
+                @click="signup()"
+                type="submit"
+                value="Sign Up here"
+                class="btn btn-lg btn-primary btn-block text-uppercase"
+              />
+            </form>
           </div>
         </div>
       </div>
-
-      <div class="half right" data-aos="fade-up">
-        <div class="container" align="center">
-          <div class="login-grid" style="padding-right:0%">
-            <input class="login-text" type="text" placeholder="Username" v-model="username" />
-            <input class="login-text" type="password" placeholder="Password" v-model="password" />
-            <input
-              @click="login(username, password)"
-              type="submit"
-              value="Login"
-              class="btn btn-primary py-2 px-4 text-white"
-            />
-            <br />
-            <span v-if="errorLogin" style="color:red">Error: {{errorLogin}}</span>
-          </div>
-        </div>
-      </div>
-
-      <br />
-      <input
-        @click="signup()"
-        type="submit"
-        value="Sign Up here"
-        class="btn btn-primary py-2 px-4 text-white"
-      />
     </div>
   </div>
+</body>
 </template>
 
-<style>
-#top-container {
+
+<style scoped>
+:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: 0.75rem;
+}
+
+body {
+  background: #687786;
+  background: linear-gradient(to right, #5f6a79, #923030);
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: 0.1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  height: auto;
+  border-radius: 2rem;
+}
+
+.form-label-group > input,
+.form-label-group > label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group > label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
   margin-bottom: 0;
-  margin-top: 0;
-  color: #d3d3d3;
-  background-color: #d3d3d3;
-  color: #ffffff;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  transition: all 0.1s ease-in-out;
 }
-#welcome h2 {
-  text-align: center;
-  margin-top: 15px;
-  margin-bottom: 10px;
-  font-size: 37px;
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
 }
-#welcome h3 {
-  text-align: center;
-  margin-top: 5px;
-  margin-bottom: 0px;
-  font-size: 30px;
-  font-family: Lucida;
-  font-style: oblique;
-  color: #d3d3d3;
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
 }
-#navbar {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-size: 20px;
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown) ~ label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+
+/* Fallback for Edge
+-------------------------------------------------- */
+
+@supports (-ms-ime-align: auto) {
+  .form-label-group > label {
+    display: none;
+  }
+  .form-label-group input::-ms-input-placeholder {
+    color: #777;
+  }
+}
+
+/* Fallback for IE
+-------------------------------------------------- */
+
+@media all and (-ms-high-contrast: none), (-ms-high-contrast: active) {
+  .form-label-group > label {
+    display: none;
+  }
+  .form-label-group input:-ms-input-placeholder {
+    color: #777;
+  }
 }
 </style>
 
