fb2cbcaa7174911d538c4e627ffc2cdff644c1cb
ssmith <sean.smith3@mail.mcgill.ca>
2019-11-27 10:41:26 -0500

    get sessions (ID)

--- TutoringSystem-Andorid/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/ViewSessionsActivity.java
+++ TutoringSystem-Andorid/app/src/main/java/ca/mcgill/ecse321/tutoringsystem/ViewSessionsActivity.java
@@ -3,7 +3,19 @@ package ca.mcgill.ecse321.tutoringsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 import java.util.List;
 
@@ -11,27 +23,71 @@ public class ViewSessionsActivity extends AppCompatActivity {
     String error = null;
 
     private List<String> sessionIDs;
-    private List<String> dates;
-    private List<String> startTimes;
-    private List<String> endTimes;
-    private List<String> tutorialID;
-    private List<String> courses;
-    private List<String> tutors;
     private ArrayAdapter<String> sessionAdapter;
-    private ArrayAdapter<String> dateAdapter;
-    private ArrayAdapter<String> startTimeAdapter;
-    private ArrayAdapter<String> endTimeAdapter;
-    private ArrayAdapter<String> tutorialAdapter;
-    private ArrayAdapter<String> courseAdapter;
-    private ArrayAdapter<String> tutorAdapter;
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_sessions);
+
+        Spinner sessionIDspinner = (Spinner) findViewById(R.id.sessionidspinner);
+        sessionAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, sessionIDs);
+        sessionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sessionIDspinner.setAdapter(sessionAdapter);
+
+        refreshLists(this.getCurrentFocus());
+
     }
+
+    /** Calls GET over here with "sessions" **/
+    public void refreshLists(View view){
+        refreshList(sessionAdapter, sessionIDs, "sessions");
+    }
+
+    private void refreshList(final ArrayAdapter<String> adapter, final List<String> names, final String restFunctionName) {
+        HttpUtils.get(
+                restFunctionName,
+                new RequestParams(),
+                new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray response){
+                        sessionIDs.clear();
+                        sessionIDs.add("Please select...");
+                        for(int i=0; i<response.length(); i++) {
+
+                            try{
+                                sessionIDs.add(response.getJSONObject(i).getString("sessionID"));
+                            }catch(Exception e) {
+                                error += e.getMessage();
+                            }
+                            refreshErrorMessage();
+                        }
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                        try{
+                            error += errorResponse.get("message").toString();
+                        }catch (JSONException e){
+                            error += e.getMessage();
+                        }
+                        refreshErrorMessage();
+                    }
+
+                });
+    }
+
+    private void refreshErrorMessage() {
+        TextView tvError = (TextView) findViewById(R.id.sessionsError);
+        tvError.setText(error);
+
+        if(error==null || error.length()==0){
+            tvError.setVisibility(View.GONE);
+        }else{
+            tvError.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
